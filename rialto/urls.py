134b9db8183bed1b9379c0d694355149461c0e55
Djenevra <begimayapsamatova@gmail.com>
2018-10-24 11:21:00 +0600

    #10 added new page atl (active tasks list)

--- rialto/urls.py
+++ rialto/urls.py
@@ -29,5 +29,6 @@ router.register('taskrelatednotes', views.TaskRelatedNotesViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url('atl/', include('atl.urls'))
 ]
