60742cf68039dc70090cc49a05b8dfb556521514
Djenevra <begimayapsamatova@gmail.com>
2018-10-25 09:57:45 +0600

    Added templates

--- atl/urls.py
+++ atl/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+app_name = 'atl'
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('<int:task_id>/', views.detail, name='detail'),
 ]
--- atl/views.py
+++ atl/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from tasks.models import Task
+
 
 def index(request):
-    return HttpResponse('Success! It works')
+    tasks_list = Task.objects.order_by('set_price')
+    context = {'tasks_list' : tasks_list}
+    return render(request, 'atl/index.html', context)
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'atl/detail.html', {'task': task})
